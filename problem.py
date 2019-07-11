93bf5acf0a5055f0262fa5cf2d769b52ba963502
devrisofstar <yh1483@naver.com>
2019-07-11 12:42:10 +0900

    .

--- problem.py
+++ problem.py
@@ -5,7 +5,7 @@ class Problem:
     def __init__(self, number, subject, info, cor, total, ratio, link=None, classify=None, is_samsung=False):
         self.number = number  # 문제 번호
         self.subject = subject  # 문제 제목
-        self.info = info  # 문제 정보 : 정보, 태그
+        self.info = info  # 문제 정보 : 정보태그
         self.cor = cor  # 정답 횟수
         self.total = total  # 제출 횟수
         self.ratio = ratio  # 정답 비율
@@ -18,6 +18,6 @@ class Problem:
         self.classify.append(classification)
 
 if __name__ == "__main__":
-    p = Problem(1, "제목", ["정보", "태그"], 100, 200, 0.2, is_samsung=True)  # example.
+    p = Problem(1, "제목", "정보태그", 100, 200, 0.2, is_samsung=True)  # example.
     p.add_classify("DFS")
     print(p.classify)
